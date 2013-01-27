11a20a83813540ba5efaedb328371b4cce5d0807
Michael Quinn <mquinn@gloto.com>
2013-01-26 23:21:21 -0500

    Use Swing layout managers to handle layout

--- src/tetris/display.clj
+++ src/tetris/display.clj
@@ -1,40 +1,19 @@
 (ns tetris.display
   (:require [tetris.colors :as colors]
             [tetris.game :as game])
-  (:import (java.awt Dimension Canvas Font)
+  (:import (java.awt Dimension Canvas Font Component)
            (java.awt.event KeyListener KeyEvent)
-           (javax.swing JFrame JPanel JLabel))
+           (javax.swing JFrame JPanel JLabel Box BoxLayout BorderFactory))
   (:use clojure.contrib.math))
 
 (def ^:private game-width 12)
 (def ^:private game-height 22)
-(def ^:private footer-height 20)
-(def ^:private header-height 20)
-(def ^:private board-outline-width 8)
+(def ^:private wall-thickness 8)
 (def ^:private board-outline-color colors/yellow)
 (def ^:private block-padding 1)
 (def ^:private block-width 20)
-(def ^:private overflow 2)
 (def ^:private padded-block (+ block-width block-padding))
-(def ^:private margin-left 30)
-(def ^:private margin-right (* padded-block 10))
-(def ^:private board-origin
-   [(+ margin-left board-outline-width block-padding) header-height])
-
-(defn- next-piece-origin
-  [screen-size]
-  [(+ (* padded-block 2)
-      (- (:width screen-size) margin-right))
-   (* padded-block 4)])
-
-(defn- game-screen-size [board]
-  (let [{{:keys [width height]} :size} board]
-    {:width (+ margin-left margin-right
-              (* 2 board-outline-width)
-              (* width (+ block-width block-padding)) block-padding)
-     :height (+ header-height footer-height
-                board-outline-width
-                (* height (+ block-width block-padding)))}))
+(def ^:private overflow 2)
 
 (defn- inset-px-rect
   "Returns the square that has been inset by a specified number of pixels"
@@ -45,53 +24,30 @@
               :height (- height (* 2 inset))}}))
 
 (defn- translate-coords
-  "Translates a tetris grid coordinate into a pixel coordinate for drawing.
-  Optionally takes the opper left pixel coordinate - but this defaults to
-  the top left of the game board."
-  ([[x y]] (translate-coords board-origin [x y]))
-  ([[grid-x grid-y] [x y]]
-   (let [x-px (+ grid-x (* x padded-block))
-         y-px (+ grid-y (* y padded-block))]
+  "Translates a tetris grid coordinate into a pixel coordinate for drawing."
+  ([[x y]]
+   (let [x-px (* x padded-block)
+         y-px (* y padded-block)]
      [x-px y-px])))
 
 (defn- piece-to-board-coords
+  "A positioned piece is defined by an offset, and a set of squares positioned
+  relative to that offset. This function returns the individual squares positioned
+  relative to the board."
   [positioned-piece]
   (let [{:keys [position piece]} positioned-piece
         [origin-x origin-y] position
         squares (:squares piece)]
     (map (fn [[x y]] [(+ x origin-x) (+ y origin-y)]) squares)))
 
-(defn- draw-walls [g board]
-  (let [{{:keys [width height]} :size} board
-        x1 margin-left
-        y1 (+ header-height (* overflow padded-block))
-        x2 (+ x1
-              block-padding
-              (* 2 board-outline-width)
-              (* width padded-block))
-        y2 (+ y1 (* (- height overflow) padded-block) board-outline-width)
-        side-bar-height (- y2 y1)
-        bottom-bar-width (- x2 x1)]
-    (.setColor g board-outline-color)
-    (.fillRect g x1 y1 board-outline-width side-bar-height)
-    (.fillRect g (- x2 board-outline-width) y1 board-outline-width side-bar-height)
-    (.fillRect g x1 (- y2 board-outline-width) bottom-bar-width board-outline-width)))
-
-(defn- draw-board [g dimensions board]
-  "Draws the Tetris game board."
-  (let [{:keys [width height]} dimensions]
-    (.setColor g colors/black)
-    (.fillRect g 0 0 width height)
-    (draw-walls g board)))
-
 (defn- draw-filled-square
-  ([g square] (draw-filled-square g square board-origin))
-  ([g square origin]
+  ([g square]
    (let [[x y] square
-         [pixel-x pixel-y] (translate-coords origin [x y])]
+         [pixel-x pixel-y] (translate-coords [x y])]
      (.fillRect g pixel-x pixel-y block-width block-width))))
 
-(defn- draw-outline-square [g square]
+(defn- draw-outline-square
+  [g square]
   (let [[x y] square
         origin-px (translate-coords [x y])
         rect-px { :origin origin-px :size { :width block-width :height block-width } }
@@ -109,18 +65,6 @@
     (doseq [square squares]
       (draw-filled-square g square))))
 
-(defn- draw-next-piece
-  [g piece origin]
-  (let [{ :keys [color squares] } piece
-        text (char-array "Next piece:")
-        [origin-x origin-y] origin]
-    (.setColor g color)
-    (doseq [square squares]
-      (draw-filled-square g square origin))
-    (.setColor g colors/white)
-    (.drawChars g text 0 (count text) (- origin-x 10) (- origin-y 15))
-    ))
-
 (defn- draw-ghost-piece
   [g positioned-piece]
   (let [{:keys [position piece]} positioned-piece
@@ -136,68 +80,33 @@
     (.setColor g color)
     (draw-filled-square g square)))
 
+(defn- draw-stats
+  [game stats-panel]
+  (let [{ :keys [score-label level-label next-piece-canvas] } stats-panel
+        buffer-strategy (.getBufferStrategy next-piece-canvas)
+        graphics (.getDrawGraphics buffer-strategy)
+        next-piece (first (:piece-bag game))]
+    (doto graphics
+      (.setColor colors/black)
+      (.fillRect 0 0 (.getWidth next-piece-canvas) (.getHeight next-piece-canvas))
+      (.setColor (:color next-piece)))
+    (doseq [square (:squares next-piece)]
+      (draw-filled-square graphics square))
+    (.dispose graphics)
+    (.show buffer-strategy)))
+
 (defn- draw-game
-  [g game]
-  (let [board (:board game)
-        screen-size (game-screen-size board)
-        dimensions screen-size
-        next-origin (next-piece-origin screen-size)]
-    (draw-board g dimensions board)
-    (draw-ghost-piece g (:ghost-piece game))
-    (draw-positioned-piece g (:current-piece game))
-    (draw-next-piece g (first (:piece-bag game)) next-origin)
-    (draw-fallen-blocks g (:state board))))
-
-(defn- observe-frame
-  [{ :keys [count last-second] }]
-  (let [now (.getTime (new java.util.Date))
-        time-elapsed (- now last-second)]
-    (if (> time-elapsed 1000)
-      { :count 0 :last-second now }
-      { :count (+ count 1) :last-second last-second })))
-
-(defn- create-label
-  [text]
-  (let [label (JLabel. text)]
-    (.setFont label (Font. "Sans Serif" Font/PLAIN 22))
-    (let [size (.getPreferredSize label)]
-      (doto label
-        (.setBounds 0 0 (.width size) (.height size))
-        (.setOpaque true)
-        (.setBackground colors/black)
-        (.setForeground colors/white)))))
-
-(defn- create-frame
-  [width-px height-px]
-  (let [frame (JFrame. "Tetris")]
-    (doto frame
-      (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
-      (.setResizable false)
-      (.setIgnoreRepaint true))
-    frame))
-
-(defn- add-game-over-message
-  [panel]
-  (let [label (create-label "Game Over")
-        board-width-px (+ (* game-width padded-block) block-padding)
-        board-height-px (+ (* game-height padded-block) block-padding)
-        board-origin-x (+ margin-left board-outline-width)
-        board-origin-y header-height
-        label-width (.getWidth label)
-        label-height (.getHeight label)
-        label-x (+ board-origin-x
-                   (floor (- (/ board-width-px 2) 
-                             (/ label-width 2))))
-        label-y (+ board-origin-y
-                   (floor (- (/ board-height-px 2)
-                             (/ label-height 2))))]
-    (.setBounds label label-x label-y label-width label-height)
-    (doto panel
-      (.add label 0)
-      (.setComponentZOrder label 0)
-      (.validate)
-      (.repaint))))
-
+  [game canvas]
+  (let [buffer-strategy (.getBufferStrategy canvas)
+        graphics (.getDrawGraphics buffer-strategy)
+        board (:board game)]
+    (.setColor graphics colors/black)
+    (.fillRect graphics 0 0 (.getWidth canvas) (.getHeight canvas))
+    (draw-positioned-piece graphics (:current-piece game))
+    (draw-ghost-piece graphics (:ghost-piece game))
+    (draw-fallen-blocks graphics (:state board))
+    (.dispose graphics)
+    (.show buffer-strategy)))
 
 (def ^:private command-key-map
   {KeyEvent/VK_LEFT  :move-left
@@ -206,59 +115,183 @@
    KeyEvent/VK_UP    :rotate-right
    KeyEvent/VK_SPACE :drop-piece})
 
-(defn- create-canvas [width-px height-px key-code-atom]
-  (let [key-listener (proxy [KeyListener][]
-                       (keyPressed [e]
-                         (if-let [command (command-key-map (.getKeyCode e))]
-                           (compare-and-set! key-code-atom @key-code-atom (conj @key-code-atom command))))
-                       (keyReleased [e]
-                         (if-let [command (command-key-map (.getKeyCode e))]
-                           (compare-and-set! key-code-atom @key-code-atom (disj @key-code-atom command))))
-                       (keyTyped [e]))
-        canvas (Canvas.)]
+(defn- create-key-listener
+  [key-code-atom]
+  (proxy [KeyListener][]
+    (keyPressed [e]
+      (if-let [command (command-key-map (.getKeyCode e))]
+        (compare-and-set! key-code-atom @key-code-atom
+                          (conj @key-code-atom command))))
+    (keyReleased [e]
+      (if-let [command (command-key-map (.getKeyCode e))]
+        (compare-and-set! key-code-atom @key-code-atom
+                          (disj @key-code-atom command))))
+    (keyTyped [e])))
+
+(defn- create-frame
+  []
+  (let [frame (JFrame. "Tetris")
+        panel (.getContentPane frame)]
+    (.setBackground panel colors/black)
+    (doto frame
+      (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+      (.setResizable false)
+      (.setIgnoreRepaint true))))
+
+(defn- create-tetris-canvas
+  [width height]
+  (let [canvas (Canvas.)
+        width-px (- (* width padded-block) block-padding)
+        height-px (- (* height padded-block) block-padding)]
     (doto canvas
       (.setBounds 0 0 width-px height-px)
-      (.setIgnoreRepaint true)
-      (.addKeyListener key-listener))
-    canvas))
+      (.setIgnoreRepaint true))))
+
+(defn- create-board-panel
+  [board canvas]
+  (let [canvas-width (.getWidth canvas)
+        canvas-height (.getHeight canvas)
+        width-px (+ (* wall-thickness 2) (* block-padding 2) canvas-width)
+        height-px (+ wall-thickness canvas-height block-padding)
+        wall-origin-y (* padded-block overflow)
+        wall-height (- height-px wall-origin-y)
+        panel (proxy [JPanel][]
+                (getPreferredSize [] (Dimension. width-px height-px))
+                (paintComponent [g]
+                  (proxy-super paintComponent g)
+                  (.setColor g board-outline-color)
+                  (.fillRect g 0 wall-origin-y wall-thickness wall-height)
+                  (.fillRect g 0 (- height-px wall-thickness) width-px wall-thickness)
+                  (.fillRect g (- width-px wall-thickness) wall-origin-y wall-thickness wall-height)))]
+    (.setBounds canvas (+ wall-thickness block-padding) 0 canvas-width canvas-height)
+    (doto panel
+      (.setBackground colors/black)
+      (.setLayout nil)
+      (.add canvas))))
+
+(defn- create-title-label
+  [text]
+  (let [label (JLabel. text)]
+    (doto label
+      (.setAlignmentX Component/LEFT_ALIGNMENT)
+      (.setFont (Font. "Sans Serif" Font/PLAIN 22))
+      (.setForeground colors/white))))
+
+(defn- create-value-label
+  [text]
+  (let [label (JLabel. text)]
+    (doto label
+      (.setFont (Font. "Sans Serif" Font/PLAIN 22))
+      (.setForeground colors/white))))
+
+(defn- create-key-and-value-row
+  "Builds two labels, set horizontally in a frame, with the
+  value label positioned on the right."
+  [title value]
+  (let [panel (JPanel.)
+        title-label (create-title-label title)
+        value-label (create-value-label value)]
+    (.setAlignmentX title-label Component/LEFT_ALIGNMENT)
+    (.setAlignmentX value-label Component/RIGHT_ALIGNMENT)
+    (doto panel
+      (.setAlignmentX Component/LEFT_ALIGNMENT)
+      (.setLayout (BoxLayout. panel BoxLayout/X_AXIS))
+      (.setBackground colors/black)
+      (.add title-label)
+      (.add (Box/createRigidArea (Dimension. 5 0)))
+      (.add (Box/createHorizontalGlue))
+      (.add value-label))
+    {:container panel
+     :value-label value-label}))
+
+(defn- create-stats-panel
+  []
+  (let [panel (JPanel.)
+        score-row (create-key-and-value-row "Score:" "0")
+        level-row (create-key-and-value-row "Level:" "1")
+        next-piece-canvas (create-tetris-canvas 4 4)
+        next-piece-panel (JPanel.)]
+    (doto next-piece-panel
+      (.setLayout nil)
+      (.setBorder (BorderFactory/createEmptyBorder 10 130 0 0))
+      (.setBackground colors/black)
+      (.add next-piece-canvas)
+      (.setPreferredSize (Dimension. (.getWidth next-piece-canvas)
+                                     (.getHeight next-piece-canvas))))
+    (doto panel
+      (.setLayout (BoxLayout. panel BoxLayout/Y_AXIS))
+      (.setBackground colors/black)
+      (.add (Box/createRigidArea (Dimension. 0 (* padded-block overflow))))
+      (.add (:container score-row))
+      (.add (:container level-row))
+      (.add (Box/createRigidArea (Dimension. 0 30)))
+      (.add (create-title-label "Next Piece:"))
+      (.add (Box/createRigidArea (Dimension. 0 15)))
+      (.add next-piece-panel))
+
+    {:score-label (:value-label score-row)
+     :level-label (:value-label level-row)
+     :next-piece-canvas next-piece-canvas
+     :container panel }
+    ))
+
+(defn- add-game-over-message
+  [panel]
+  (let [label (JLabel. "Game Over")]
+    (doto label
+      (.setFont (Font. "Sans Serif" Font/PLAIN 32))
+      (.setBackground colors/black)
+      (.setOpaque true)
+      (.setForeground colors/white))
+    (let [label-width (.getWidth (.getPreferredSize label))
+          label-height (.getHeight (.getPreferredSize label))
+          label-x (+ (floor (- (/ (.getWidth panel) 2) 
+                               (/ label-width 2))))
+          label-y (+ (floor (- (/ (.getHeight panel) 2)
+                               (/ label-height 2))))]
+      (.setBounds label label-x label-y label-width label-height)
+      (doto panel
+        (.add label 0)
+        (.setComponentZOrder label 0)
+        (.validate)
+        (.repaint)))))
 
 (defn tetris-swing []
   "Starts a tetris game drawn using Java Swing"
   (let [game (game/new-game game-width game-height)
         board (:board game)
-        { width-px :width height-px :height } (game-screen-size board)
+        {{ board-width :width board-height :height } :size} board
         frame-counter { :count 0 :last-second (.getTime (new java.util.Date)) }
         key-code-atom (atom #{})
-        frame (create-frame width-px height-px)
-        panel (.getContentPane frame)
-        canvas (create-canvas width-px height-px key-code-atom)]
-    (doto panel
-      (.setPreferredSize (Dimension. width-px height-px))
-      (.setLayout nil)
-      (.add canvas))
+        frame (create-frame)
+        canvas (create-tetris-canvas board-width board-height)
+        board-panel (create-board-panel board canvas)
+        stats-panel (create-stats-panel)
+        content-panel (.getContentPane frame)]
+    (doto content-panel
+      (.setLayout (BoxLayout. content-panel BoxLayout/X_AXIS))
+      (.setBorder (BorderFactory/createEmptyBorder 20 20 20 20))
+      (.add board-panel)
+      (.add (Box/createRigidArea (Dimension. 20 0)))
+      (.add (:container stats-panel)))
     (doto frame
       (.setVisible true)
+      (.addKeyListener (create-key-listener key-code-atom))
+      (.requestFocus)
       (.pack))
-    (doto canvas
-      (.createBufferStrategy 2)
-      (.requestFocus))
-    (loop [game (game/start game (System/currentTimeMillis))
-           frame-counter frame-counter]
+    (.createBufferStrategy canvas 2)
+    (.createBufferStrategy (:next-piece-canvas stats-panel) 2)
+    (loop [game (game/start game (System/currentTimeMillis))]
       (let [current-time (System/currentTimeMillis)
-            game (game/step game @key-code-atom current-time)
-            frame-counter (observe-frame frame-counter)
-            buffer-strategy (.getBufferStrategy canvas)
-            graphics (.getDrawGraphics buffer-strategy)]
-          (draw-game graphics game)
-          (.dispose graphics)
-          (.show buffer-strategy)
-          (Thread/sleep 10)
-          (if (= (:status game) :dropping)
-            (recur game frame-counter))))
-    (add-game-over-message panel)
+            game (game/step game @key-code-atom current-time)]
+        (draw-game game canvas)
+        (draw-stats game stats-panel)
+        (Thread/sleep 10)
+        (if (= (:status game) :dropping)
+          (recur game))))
+    (add-game-over-message board-panel)
     (loop []
       (Thread/sleep 10)
-      (recur))
-    ))
+      (recur))))
 
 
